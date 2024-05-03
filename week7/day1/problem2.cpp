1b5f667105741538a40993d24cfdd28ae67d82bb
sandipondas <163525455+sandipondas@users.noreply.github.com>
2024-05-03 22:44:06 +0600

    Add files via upload

---
+++ week7/day1/problem2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl "\n"
+#define all(x) x.begin(), x.end()
+
+const int mod = 1e9 + 7;
+const double pi = 2 * acos(0);
+int Max = INT_MAX;
+int Min = INT_MIN;
+int Inf = 1e18;
+using namespace std;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tt = 1;
+    //cin >> tt;
+    while (tt--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> v(n);
+        for(int i=0; i<n; i++) cin>>v[i];
+        
+        while(k--)
+        {
+            int a;
+            cin>>a;
+            int l=0,r=n-1;
+            int ans=-1;
+            while(l<=r)
+            {
+                int mid=(l+r)/2;
+                if(a>=v[mid])
+                {
+                    ans=mid;
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
+            cout<<ans+1<<nl;
+
+        }
+    }
+    return 0;
+}
