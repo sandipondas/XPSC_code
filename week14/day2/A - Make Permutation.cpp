0c159fc9ae5a2dc9ae0bf36c870e10d87313f00e
sandipondas <163525455+sandipondas@users.noreply.github.com>
2024-06-30 11:01:34 +0600

    Add files via upload

---
+++ week14/day2/A - Make Permutation.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl "\n"
+#define all(x) x.begin(), x.end()
+#define rasa ios_base::sync_with_stdio(false); cin.tie(0);cout.tie(0);
+
+
+int Max = INT_MAX;
+int Min = INT_MIN;
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using pbds= tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+//less(set); less_equal(multyset)
+int32_t main()
+{
+    rasa
+    int tt=1;
+    cin>>tt;
+    while(tt--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+        sort(all(v));
+        reverse(all(v));
+
+        // for(auto val:v)
+        // cout<<val<<" ";
+        // cout<<nl;
+
+        bool ans=true;
+        int x=n;
+        for(auto val : v)
+        {
+            if(val<=x)
+            {
+                x--;
+            }
+            else 
+            {
+                ans=false;
+                break;
+            }
+        }
+        if(ans) yes;
+        else no;
+    }
+    return 0;
+}
---
+++ week14/day2/B - Trick Or Treat.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl "\n"
+#define all(x) x.begin(), x.end()
+#define rasa ios_base::sync_with_stdio(false); cin.tie(0);cout.tie(0);
+
+
+int Max = INT_MAX;
+int Min = INT_MIN;
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> using pbds= tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
+//less(set); less_equal(multyset)
+int32_t main()
+{
+    rasa
+    int tt=1;
+    cin>>tt;
+    while(tt--)
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<int> cho(n),cnd(n);
+        for(int i=0; i<n; i++) cin>>cho[i];
+        for(int i=0; i<n; i++) cin>>cnd[i];
+
+        map<int,int> mp;
+
+        for(int i=0; i<n; i++)
+        {
+            int x=cho[i]%m;
+            mp[x]++;
+            // cout<<x<<" ";
+        }
+        // cout<<nl;
+        // for(auto val : mp)
+        // {
+        //     cout<<val.first<<" "<<val.second<<nl;
+        // }
+        // cout<<nl;
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+            int x=cnd[i]%m;
+            int y=m-x;
+            if(x==0)
+            {
+                ans+=mp[x];
+            }
+            else
+            {
+                ans+=mp[y];
+            }
+            // cout<<x<<" ";
+            
+        }
+        cout<<ans<<nl;
+    }
+    return 0;
+}
